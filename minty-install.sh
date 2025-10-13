81c71b22ead96af9350fd1b9f41eff826c37ed80
Mason Jennings <masonj@csu.fullerton.edu>
2025-10-13 09:24:25 -0700

    add basic install script

--- minty-install.sh
+++ minty-install.sh
@@ -1,3 +1,93 @@
 #!/bin/bash
 
-echo "Hello world!"
+#   *************************************************************************************#
+#   MyMintySetup
+#   Author: Mason Jennings
+#   GitHub: https://github.com/masonj5002/my-minty-setup
+#   *************************************************************************************#
+#   This script automates basic software install and config settings on Linux Mint 22.2  
+#   Cinnamon (and similar) systems.
+#   *************************************************************************************#
+
+#   --- Variables ---   #
+APT_REMOVE_PACKAGES=(
+    "cups-browsed"
+)
+
+APT_INSTALL_PACKAGES=(
+    "chromium"
+    "spotify-client"
+    "ttf-mscorefonts-installer"
+    "gimp"
+    "git"
+    "clang"
+    "clang-format"
+    "numlockx"                          # *Enables numlock on boot
+    "neofetch"                          # *24.04 and lower, use `fastfetch` for higher
+    "curl"                              # *Already installed on 22.2
+    "flatpak"                           # *Already installed on 22.2
+)
+
+FLATPAK_APPS_FLATHUB=(
+    "com.github.tchx84.Flatseal"
+    "com.usebottles.bottles"
+    "org.kde.kolourpaint"
+    "org.remmina.Remmina"
+)
+
+NOT_DEB_FILES_TO_DOWNLOAD=(
+    "https://www.wizard101.com/downloadGame/OtherDownload"
+    ""
+)
+
+DEBS_TO_DOWNLOAD=(
+    "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+    "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+)
+
+FLATPAKREF_FILES_TO_DOWNLOAD=(
+    "https://cdn.toontownrewritten.com/launcher/linux/launcher.flatpakref"
+)
+
+update_system() {
+    echo "**Updating package lists..."
+    apt update
+    echo "**Upgrading system"
+    apt upgrade
+}
+
+install_apt_packages() {
+    echo "**Updating package lists..."
+    apt update
+
+    for pkg in "$APT_INSTALL_PACKAGES"; do
+        echo "**Installing $pkg..."
+        apt install $pkg -y
+    done
+}
+
+#   --- MAIN ---   #
+echo "**Welcome, $(whoami)!**
+"
+sleep 0.5
+echo "**Starting Linux Mint script setup...**"
+sleep 0.5
+sudo apt
+sleep 0.5
+
+echo "
+**Installing Updates...**"
+sleep 0.5
+update_system
+sleep 0.5
+
+echo "
+**Installing APT Packages**"
+sleep 0.5
+install_apt_packages
+sleep 0.5
+
+echo "
+**Complete.**
+**Enjoy your machine!**"
+sleep 1
